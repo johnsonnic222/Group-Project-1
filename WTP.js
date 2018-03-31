cc04d68f242d247b504e516f2af23c4407720920
Nicole Johnson <johnsonbadgersnic222@gmail.com>
2018-03-31 11:23:45 -0400

    2

--- WTP.js
+++ WTP.js
@@ -1,128 +1,68 @@
-// SETUP VARIABLES
-// ==========================================================
-
-// This variable will be pre-programmed with our authentication key (the one we received when we registered)
 var authKey = "sa7Im6LlV1A14WoGvUiRHp28lycfPPy6904zJ4En";
-
-// These variables will hold the results we get from the user's inputs via HTML
-var queryTerm 	= "";
-
-
-// Based on the queryTerm we will create a queryURL 
+var queryTerm = "";
 var queryURLBase = "https://api.whitehouse.gov/v1/petitions.json?";
-
-// Array to hold the various article info
-var articleCounter = 0;
-
-// FUNCTIONS
-// ==========================================================
+var petitionCounter = 0;
 
 
-// This runQuery function expects two parameters (the number of articles to show and the final URL to download data from)
-function runQuery(numArticles, queryURL){
+function runQuery(numPetitions, queryURL) {
 
-	// The AJAX function uses the URL and Gets the JSON data associated with it. The data then gets stored in the variable called: "NYTData"
-	$.ajax({url: queryURL, method: "GET"}) 
-		.done(function(NYTData) {
+	$.ajax({ url: queryURL, method: "GET" })
+	.done(function (WTPData) {
 
-			// Here we are logging the URL so we have access to it for troubleshooting
 			console.log("------------------------------------")
 			console.log("URL: " + queryURL);
 			console.log("------------------------------------")
 
-			// Here we then log the NYTData to console, where it will show up as an object.
-			console.log(NYTData);
+			console.log(WTPData);
 			console.log("------------------------------------")
 
-			// Loop through and provide the correct number of articles
-			for (var i=0; i<numArticles; i++) {
+	
+		for (var i = 0; i < numPetitions; i++) {
+			petitionCounter++;
 
-					// Add to the Article Counter (to make sure we show the right number)
-					articleCounter++;
+			
+			var wellSection = $("<div>");
+			wellSection.addClass('well');
+			wellSection.attr('id', 'petitionWell-' + petitionCounter)
+			$('#wellSection').append(wellSection);
 
-					// Create the HTML Well (Section) and Add the Article content for each
-					var wellSection = $("<div>");
-					wellSection.addClass('well');
-					wellSection.attr('id', 'articleWell-' + articleCounter)
-					$('#wellSection').append(wellSection);
+			if (WTPData.results[i].title != "null") {
+				$("#petitionWell-" + petitionCounter).append('<h3><span class="label label-primary">' + petitionCounter + '</span><strong>   ' + WTPData.results[i].title + "</strong></h3>");
+				$("#petitionWell-" + petitionCounter).append("<a href='" + WTPData.results[i].url + "'>" + WTPData.results[i].url + "</a>");
 
-					// Confirm that the specific JSON for the article isn't missing any details
-					// If the article has a headline include the headline in the HTML
-					if(NYTData.response.docs[i].headline != "null")
-					{
-						$("#articleWell-"+ articleCounter).append('<h3><span class="label label-primary">' + articleCounter + '</span><strong>   ' + NYTData.response.docs[i].headline.main + "</strong></h3>");
-						
-						// Log the first article's Headline to console.
-						console.log(NYTData.response.docs[i].headline.main);
-					}
-					
-					// If the article has a Byline include the headline in the HTML
-					if( NYTData.response.docs[i].byline && NYTData.response.docs[i].byline.hasOwnProperty("original"))
-					{
-						$("#articleWell-"+ articleCounter).append('<h5>' + NYTData.response.docs[i].byline.original + "</h5>");
-
-						// Log the first article's Author to console.
-						console.log(NYTData.response.docs[i].byline.original);
-					}
-
-					// Then display the remaining fields in the HTML (Section Name, Date, URL)
-					$("#articleWell-"+ articleCounter).append('<h5>Section: ' + NYTData.response.docs[i].section_name + "</h5>");
-					$("#articleWell-"+ articleCounter).append('<h5>' + NYTData.response.docs[i].pub_date + "</h5>");
-					$("#articleWell-"+ articleCounter).append("<a href='" + NYTData.response.docs[i].web_url + "'>" + NYTData.response.docs[i].web_url + "</a>");
-
-					// Log the remaining fields to console as well
-					console.log(NYTData.response.docs[i].pub_date);
-					console.log(NYTData.response.docs[i].section_name);
-					console.log(NYTData.response.docs[i].web_url);	
+				// Log the first article's Headline to console.
+				console.log(WTPData.results[i].title);
+				console.log(WTPData.results[i].url);
 			}
-		});
+
+			// Then display the remaining fields in the HTML (Section Name, Date, URL)
+			//$("#petitionWell-" + petitionCounter).append("<a href='" + WTPData.results[i].url + "'>" + WTPData.results[i].isSignable + "</a>");
+			//$("#petitionWell-" + petitionCounter).append("<a href='" + WTPData.results[i].url + "'>" + WTPData.results[i].signatureCount + "</a>");
+			// $("#petitionWell-" + petitionCounter).append("<a href='" + WTPData.results[i].url + "'>" + WTPData.results[i].url + "</a>");
+
+			// Log the remaining fields to console as well
+			//console.log(WTPData.results[i].pub_date);
+			//console.log(WTPData.results[i].section_name);
+			console.log(WTPData.results[i].url);
+		}
+	});
 
 }
 
-// METHODS
-// ==========================================================
-	
-	// On Click button associated with the Search Button
-	$('#runSearch').on('click', function(){
-        event.preventDefault();
+$('#runSearch').on('click', function () {
+	event.preventDefault();
 
+	petitionCounter = 0;
+	$("#wellSection").empty();
 
-		// Initially sets the articleCounter to 0
-		articleCounter = 0;
+	var searchTerm = $('#searchTerm').val().trim();
+    queryURL = queryURLBase + "&title=" + searchTerm + "&body=" + searchTerm;
 
-		// Empties the region associated with the articles
-		$("#wellSection").empty();
+	numResults = $("#numRecordsSelect").val();
 
-		// Search Term
-        var searchTerm = $('#searchTerm').val().trim();
-        
-		queryURL = queryURLBase + "&title=" + searchTerm + "&body=" + searchTerm;
+	runQuery(numResults, queryURL);
 
-		// Num Results
-		numResults = $("#numRecordsSelect").val();
-
-		// // Start Year
-		// startYear = $('#startYear').val().trim();
-
-		// // End Year
-		// endYear = $('#endYear').val().trim();
-
-		// If the user provides a startYear -- the startYear will be included in the queryURL
-	// 	if (parseInt(startYear)) {
-	// 		queryURL = queryURL + "&begin_date=" + startYear + "0101";
-	// 	}
-
-	// 	// If the user provides a startYear -- the endYear will be included in the queryURL
-	// 	if (parseInt(endYear)) {
-	// 		queryURL = queryURL + "&end_date=" + endYear + "0101";
-	// 	}
-
-	// 	// Then we will pass the final queryURL and the number of results to include to the runQuery function
-		runQuery(numResults, queryURL);
-
-	// 	// This line allows us to take advantage of the HTML "submit" property. This way we can hit enter on the keyboard and it registers the search (in addition to clicks).
-	// 	return false;
-	});	
+});
 
 // This button clears the top articles section
 // $('#clearAll').on('click', function(){
